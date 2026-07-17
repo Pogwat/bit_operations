546f9f7da7ab6528def5216da67077f82241451a
Pogwat <208688662+Pogwat@users.noreply.github.com>
2026-07-17 06:35:49 -0400

    inline everything

--- src/lib.rs
+++ src/lib.rs
@@ -7,35 +7,44 @@ pub trait BitOps:IntOps { //Todo: Make this trait const when #143874 is staballi
     /// number of bits required to address a bit in this type
     const BIT_BITS:u32 = Self::BITS.ilog2();
     /// Generate a bitmask for aa range of bits
+    #[inline]
     fn bitmask<R:BitZRange<Self> >(range:&R) -> Self { //indexes: 0..=Self::BITS-1
         let start = range.bits_start();
         let end = range.bits_end();
         (!Self::ZERO >> (Self::BITS as u8 - 1 - (end - start))) << start
     }
     /// Get a specifc bit by bit index (0 indexed)
+    #[inline]
     fn get_bit(&self, bitdex:u8) -> bool {(*self & Self::ONE<<bitdex) !=Self::ZERO }
     /// Set a specifc bit by bit index (0 indexed)
+    #[inline]
     fn set_bit(&mut self, bitdex:u8, val:bool) {
         let mask = Self::ONE<<bitdex;
         *self = (*self & !mask) | Self::from(val)<<bitdex; //Clear bit then set it
     }
     /// Get bits in range (0 indexed)
+    #[inline]
     fn get_bits<R:BitZRange<Self> >(&self, range:&R) -> Self {Self::bitmask(range) & *self}
     /// count 0 bits in range (0 indexed)
+    #[inline]
     fn ctz<R:BitZRange<Self> >(&self, range:&R) -> u8 {
         ((!Self::bitmask(range)) | *self).count_zeros() as u8 //  111111BitsWeWant1111111 , others are 1
     }
     /// count 1 bits in range (0 indexed)
+    #[inline]
     fn popcnt<R:BitZRange<Self> >(&self, range:&R) -> u8 {
         self.get_bits(range).count_ones() as u8
     }
     /// set bits in range to val (0 indexed)
+    #[inline]
     fn set_bits<R:BitZRange<Self> >(&mut self, range:&R, val:bool) {
         self.set_these_bits(Self::set_all_bit(val),range)
     }
     /// set all bits to val
+    #[inline]
     fn set_all_bit(val:bool) -> Self {(Self::ZERO).wrapping_sub(Self::from(val))}
     /// set a specfic range of self to these bits (0 indexed)
+    #[inline]
     fn set_these_bits<R:BitZRange<Self> >(&mut self, bits:Self, range:&R) {
         //XOR is commutative and self-inverse
         //A ^B ^B  = A ^(B^B), B^B = 0, So A^B^B = A , dobule xoring undos xor
@@ -44,23 +53,28 @@ pub trait BitOps:IntOps { //Todo: Make this trait const when #143874 is staballi
         *self ^= diff; //XORing the diff undo the xor leaving just a truncated bits and self
     }
     /// get mutable ref to type using proxy, MUST DROP REF FOR BIT TO UPDATE!!!!
+    #[inline]
     fn mut_bit(&mut self, bit:u8) -> MutBitProxy<'_,Self> {MutBitProxy::<Self>::new(self,bit)} //Returns proxy struct, on drop proxy updates bit
     /// first one in a bit range
+    #[inline]
     fn first_one<R:BitZRange<Self>>(&self, range:&R) -> Option<u8> {
         let masked =  *self & Self::bitmask(range); //0000BitsWeWant0000
        (masked!=Self::ZERO).then(|| masked.trailing_zeros() as u8) //Then is lazy
     }
     /// last one in a bit range
+    #[inline]
     fn last_one<R:BitZRange<Self>>(&self, range:&R) -> Option<u8> {
         let masked =  *self & Self::bitmask(range); //0000BitsWeWant0000
         (masked!=Self::ZERO).then(|| (Self::BITS-1 - masked.leading_zeros()) as u8)
     }
     /// first zero in a bit range
+    #[inline]
     fn first_zero<R:BitZRange<Self>>(&self, range:&R) -> Option<u8> {
         let masked =  *self | !Self::bitmask(range); //1111BitsWeWant1111
        (masked!=!Self::ZERO).then(|| masked.trailing_ones() as u8)
     }
     /// last zero in a bit range
+    #[inline]
     fn last_zero<R:BitZRange<Self>>(&self, range:&R) -> Option<u8> {
         let masked =  *self | !Self::bitmask(range); //1111BitsWeWant1111
        (masked!=!Self::ZERO).then(|| (Self::BITS-1 - masked.leading_ones()) as u8)
